6fca409c6d40cd3dab90d9ddfe96fc130729eba3
nhatduydc <nhatduydc@gmail.com>
2023-11-05 17:03:41 +0700

    upload web laptop

---
+++ inc/slide.php
@@ -0,0 +1,22 @@
+<?php
+$slider = new Slider();
+$list = $slider->getAllNoLimit();
+
+?>
+
+
+<div class="slider-area">
+	<div class="block-slider block-slider4">
+		<ul class="" id="bxslider-home4">
+			<?php
+			foreach ($list as $r) {
+			?>
+				<li>
+					<img  src="<?php echo 'admin/slider/uploads/' . $r['img'] ?>" alt="Slide">
+				</li>
+			<?php
+			}
+			?>
+		</ul>
+	</div>
+</div> 
