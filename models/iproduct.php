7a756399d17287b6e0558c3769a00ec6cf911584
nhatduydc <nhatduydc@gmail.com>
2023-11-05 16:54:52 +0700

    update web

--- models/iproduct.php
+++
@@ -1,9 +0,0 @@
-<?php
-interface Iproduct
-{ 
-    function getAll($offset, $count);
-    function insert($payload,$src,$srcOfContent);
-    function delete($id);
-    function update($payload);
-    function getProductById($id);
-}
