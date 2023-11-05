376ca3f6b5147415ef3ec83697bd0403e4393a3a
nhatduydc <nhatduydc@gmail.com>
2023-11-05 16:56:33 +0700

    trang chu

---
+++ inc/promo.php
@@ -0,0 +1,32 @@
+
+<div class="promo-area">
+    <div class="zigzag-bottom"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="single-promo promo1">
+                    <i class="fa fa-refresh"></i>
+                    <p>Bảo hành 30 ngày</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="single-promo promo2">
+                    <i class="fa fa-truck"></i>
+                    <p>Free shipping</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="single-promo promo3">
+                    <i class="fa fa-lock"></i>
+                    <p>Thanh toán dễ dàng</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="single-promo promo4">
+                    <i class="fa fa-gift"></i>
+                    <p>Sản phẩm mới</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
