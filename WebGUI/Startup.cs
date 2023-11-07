0e1677425b28456442c36bdd1e433a085bc17033
nhatduydc <nhatduydc@gmail.com>
2023-11-07 15:16:12 +0700

    Trang Chu

---
+++ WebGUI/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebGUI.Startup))]
+namespace WebGUI
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
--- models/imodel.php
+++
@@ -1,9 +0,0 @@
-<?php
-interface Imodel
-{ 
-    function getAll($offset, $count);
-    function insert($payload);
-    function delete($id);
-    function update($payload);
-    function getById($id);
-}
