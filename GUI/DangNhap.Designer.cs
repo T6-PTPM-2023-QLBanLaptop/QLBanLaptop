cba8b54efcdc71d7e1affe6095c85a176f7544a9
nhatduydc <nhatduydc@gmail.com>
2023-11-07 16:59:57 +0700

    update login

--- GUI/DangNhap.Designer.cs
+++ GUI/DangNhap.Designer.cs
@@ -28,11 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_huy = new System.Windows.Forms.Button();
+            this.btn_dangnhap = new System.Windows.Forms.Button();
+            this.txt_matkhau = new System.Windows.Forms.TextBox();
+            this.txt_tentaikhoan = new System.Windows.Forms.TextBox();
+            this.lbl_matkhau = new System.Windows.Forms.Label();
+            this.lbl_tennguoidung = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_huy
+            // 
+            this.btn_huy.Location = new System.Drawing.Point(291, 132);
+            this.btn_huy.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_huy.Name = "btn_huy";
+            this.btn_huy.Size = new System.Drawing.Size(100, 28);
+            this.btn_huy.TabIndex = 11;
+            this.btn_huy.Text = "Hủy bỏ";
+            this.btn_huy.UseVisualStyleBackColor = true;
+            this.btn_huy.Click += new System.EventHandler(this.btn_huy_Click);
+            // 
+            // btn_dangnhap
+            // 
+            this.btn_dangnhap.Location = new System.Drawing.Point(163, 132);
+            this.btn_dangnhap.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_dangnhap.Name = "btn_dangnhap";
+            this.btn_dangnhap.Size = new System.Drawing.Size(100, 28);
+            this.btn_dangnhap.TabIndex = 10;
+            this.btn_dangnhap.Text = "Đăng nhập";
+            this.btn_dangnhap.UseVisualStyleBackColor = true;
+            this.btn_dangnhap.Click += new System.EventHandler(this.btn_dangnhap_Click);
+            // 
+            // txt_matkhau
+            // 
+            this.txt_matkhau.Location = new System.Drawing.Point(163, 80);
+            this.txt_matkhau.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_matkhau.Name = "txt_matkhau";
+            this.txt_matkhau.PasswordChar = '●';
+            this.txt_matkhau.Size = new System.Drawing.Size(227, 22);
+            this.txt_matkhau.TabIndex = 9;
+            // 
+            // txt_tentaikhoan
+            // 
+            this.txt_tentaikhoan.Location = new System.Drawing.Point(163, 37);
+            this.txt_tentaikhoan.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_tentaikhoan.Name = "txt_tentaikhoan";
+            this.txt_tentaikhoan.Size = new System.Drawing.Size(227, 22);
+            this.txt_tentaikhoan.TabIndex = 8;
+            // 
+            // lbl_matkhau
+            // 
+            this.lbl_matkhau.AutoSize = true;
+            this.lbl_matkhau.Location = new System.Drawing.Point(45, 84);
+            this.lbl_matkhau.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_matkhau.Name = "lbl_matkhau";
+            this.lbl_matkhau.Size = new System.Drawing.Size(66, 17);
+            this.lbl_matkhau.TabIndex = 7;
+            this.lbl_matkhau.Text = "Mật khẩu";
+            // 
+            // lbl_tennguoidung
+            // 
+            this.lbl_tennguoidung.AutoSize = true;
+            this.lbl_tennguoidung.Location = new System.Drawing.Point(45, 40);
+            this.lbl_tennguoidung.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_tennguoidung.Name = "lbl_tennguoidung";
+            this.lbl_tennguoidung.Size = new System.Drawing.Size(71, 17);
+            this.lbl_tennguoidung.TabIndex = 6;
+            this.lbl_tennguoidung.Text = "Tài khoản";
+            // 
+            // DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 202);
+            this.Controls.Add(this.btn_huy);
+            this.Controls.Add(this.btn_dangnhap);
+            this.Controls.Add(this.txt_matkhau);
+            this.Controls.Add(this.txt_tentaikhoan);
+            this.Controls.Add(this.lbl_matkhau);
+            this.Controls.Add(this.lbl_tennguoidung);
+            this.Name = "DangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DangNhap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_huy;
+        private System.Windows.Forms.Button btn_dangnhap;
+        private System.Windows.Forms.TextBox txt_matkhau;
+        private System.Windows.Forms.TextBox txt_tentaikhoan;
+        private System.Windows.Forms.Label lbl_matkhau;
+        private System.Windows.Forms.Label lbl_tennguoidung;
     }
 }
--- GUI/DangNhap.cs
+++ GUI/DangNhap.cs
@@ -7,14 +7,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DTO;
+using BLL;
 
 namespace GUI
 {
     public partial class DangNhap : Form
     {
+        TaiKhoan taikhoan = new TaiKhoan();
+        TaiKhoanBLL tkbll = new TaiKhoanBLL();
+
         public DangNhap()
         {
             InitializeComponent();
+        }
+
+        private void btn_dangnhap_Click(object sender, EventArgs e)
+        {
+            taikhoan.TenTaiKhoan = txt_tentaikhoan.Text;
+            taikhoan.MatKhau = txt_matkhau.Text;
+
+            string getuser = tkbll.CheckLogin(taikhoan);
+
+            switch (getuser)
+            {
+                case "requeid_taikhoan":
+                    MessageBox.Show("Tài khoản không được để trống!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+
+                case "requeid_password":
+                    MessageBox.Show("Mật khẩu không được để trống!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+
+                case "Tài khoản hoặc mật khẩu không chính xác!":
+                    MessageBox.Show("Tài khoản hoặc mật khẩu không chính xác!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+            }
+
+            MessageBox.Show("Đăng nhập thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            TrangChu trangChu = new TrangChu();
+            trangChu.Show();
+            this.Hide();
+        }
+
+        private void btn_huy_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
